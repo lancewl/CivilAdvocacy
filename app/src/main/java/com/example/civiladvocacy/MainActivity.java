b95a32335628ab90fcf087e3757ffc2279ed2c81
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-04-06 17:15:08 -0500

    Add menu btn

--- app/src/main/java/com/example/civiladvocacy/MainActivity.java
+++ app/src/main/java/com/example/civiladvocacy/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.civiladvocacy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.info_btn) {
+            // TODO
+            return true;
+        } else if (item.getItemId() == R.id.location_btn) {
+            // TODO
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
